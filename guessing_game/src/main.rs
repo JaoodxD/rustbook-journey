faad976acd476d9d6af1f3c2d95db4a09207c30a
JaoodxD <jaood96@gmail.com>
2024-06-15 20:49:29 +0300

    Remove unnecessary code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,13 +6,10 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is {secret_number}");
-
     let mut guess = String::new();
 
     let _ = std::io::stdin()
         .read_line(&mut guess);
-        // .expect("Failed to read line");
 
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
